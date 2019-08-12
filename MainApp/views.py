3652e6c0f2952204c8c165e637ee9c0f0ec968f4
akuksov <kuksoff.alexey@gmail.com>
2019-08-12 23:36:28 +0300

    darts template

--- MainApp/views.py
+++ MainApp/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     player_1 = 'Алексей'
     player_2 = 'Дмитрий'
-    return render(request, 'MainApp/wrapper.html', context={'player_1': player_1,
-                                                            'player_2': player_2})
+    return render(request, 'MainApp/main.html', context={'player_1': player_1,
+                                                         'player_2': player_2})
