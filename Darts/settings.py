883985deaf99effb5300583fa163d441f7ee378f
akuksov <kuksoff.alexey@gmail.com>
2019-08-15 21:18:15 +0300

    Databases

--- Darts/settings.py
+++ Darts/settings.py
@@ -74,10 +74,12 @@ WSGI_APPLICATION = 'Darts.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {'default': dj_database_url.config(default='postgres://user:pass@localhost/dbname')}
+DATABASES['default'].update(db_from_env)
 # DATABASES = {'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))}
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
+# DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
